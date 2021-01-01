78f93337a3d5350140813331af1df8cecdb52e90
justingodden <justingodden@gmail.com>
2021-01-01 20:23:24 +0000

    change range input to select input

--- src/components/ValueRange.js
+++ src/components/ValueRange.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
+function range(start, stop) {
+    let number_array = [];
+    for (let i=start; i < (stop+1); i++) {
+        number_array.push(i);
+    }
+    return number_array;
+}
+
 export default function ValueRange({ name, values }) {
+    const arr = range(values[0], values[1])
     return (
         <form>
             <h1>{ name }</h1>
-            <input
-                type="range"
-                name={ name }
-                id={ name }
-                min={ values[0] }
-                max={ values[1] }
-            />
+            <select name={ name } id={ name }>
+                { arr.map((value, i) => (
+                    <option value={value}>{value}</option>
+                    ))
+                }
+            </select>
         </form>
     )
 }
